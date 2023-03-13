c05f51fcf353db1834a6d4bf699eead7ce58ef60
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-03-13 17:26:30 +0300

    feat: model classes

---
+++ src/product/product.model/product.model.ts
@@ -0,0 +1,16 @@
+export class ProductModel {
+  images: string;
+  title: string;
+  price: number;
+  oldPrice: number;
+  credit: number;
+  calculatedRating: number;
+  description: string;
+  advantages: string;
+  disadvantages: string;
+  categories: string[];
+  tags: string;
+  characteristics: {
+    [key: string]: string;
+  };
+}
