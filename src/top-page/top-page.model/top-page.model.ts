89af9a30e56969c4d16a72e432ae555223dbd58c
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-09 00:13:04 +0300

    feat: top page model

--- src/top-page/top-page.model/top-page.model.ts
+++ src/top-page/top-page.model/top-page.model.ts
@@ -1,3 +1,8 @@
+import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import { prop } from "@typegoose/typegoose";
+
+
+
 export enum TopLevelCategory {
   Courses,
   Services,
@@ -5,23 +10,60 @@ export enum TopLevelCategory {
   Products
 }
 
-export class TopPageModel {
-  _id: string;
-  firstLevel: TopLevelCategory;
-  secondCategory: string;
+export class HhData {
+
+  @prop()
+  count: number;
+
+  @prop()
+  juniorSalary: number;
+
+  @prop()
+  middleSalary: number;
+
+  @prop()
+  seniorSalary: number;
+}
+
+export class TopPageAdvantage {
+
+  @prop()
   title: string;
+
+  @prop()
+  description: string;
+}
+
+export interface TopPageModel extends Base {}
+export class TopPageModel extends TimeStamps {
+
+  @prop({ enum: TopLevelCategory})
+  firstLevel: TopLevelCategory;
+
+  @prop()
+  secondCategory: string;
+
+  @prop({ unique: true })
+  alias: string;
+  
+  @prop()
+  title: string;
+
+  @prop()
   category: string;
-  hh?: {
-    count: number;
-    juniorSalary: number;
-    middleSalary: number;
-    seniorSalary: number;
-  };
-  advantages: {
-    title: string;
-    description: string;
-  }[];
+
+  @prop({ type: () => HhData, _id: false })
+  hh?: HhData;
+
+  @prop({ type: () => [TopPageAdvantage], _id: false })
+  advantages: TopPageAdvantage[];
+
+  @prop()
   seoText: string;
+
+  @prop()
   tagsTitle: string;
+
+  @prop({ type: () => [String] })
   tags: string[];
 }
