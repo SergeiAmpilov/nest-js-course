ab3317934c404677b58d6c6594865b1b9965846f
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-05-08 23:53:51 +0300

    feat: product model

--- src/product/product.model/product.model.ts
+++ src/product/product.model/product.model.ts
@@ -1,17 +1,52 @@
-export class ProductModel {
-  _id: number;
+import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import { prop } from "@typegoose/typegoose";
+
+export interface ProductModel extends Base {}
+
+class ProductCharacteristic {
+  @prop()
+  name: string;
+
+  @prop()
+  value: string;
+}
+
+
+export class ProductModel extends TimeStamps{
+
+  @prop()
   image: string;
+
+  @prop()
   title: string;
+
+  @prop()
   price: number;
+
+  @prop()
   oldPrice: number;
+
+  @prop()
   credit: number;
+
+  @prop()
   calculatedRating: number;
+
+  @prop()
   description: string;
+
+  @prop()
   advantages: string;
+
+  @prop()
   disadvantages: string;
+
+  @prop({ type: () => [String] })
   categories: string[];
+
+  @prop({ type: () => [String] })
   tags: string[];
-  characteristics: {
-    [key: string]: string;
-  };
+
+  @prop({ type: () => [ProductCharacteristic], _id: false })
+  characteristics: ProductCharacteristic[];
 }
